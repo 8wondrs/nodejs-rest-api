75d33af5ee08b7335a8bf959f700c8f3638c2e1b
8wondrs <8wondrs@gmail.com>
2023-08-09 21:30:24 +0300

    fix

--- helpers/validateBody.js
+++ helpers/validateBody.js
@@ -4,13 +4,14 @@ const validateBody = schema => {
   const func = (req, res, next) => {
     const { error, value } = schema.validate(req.body);
 
+    if (Object.keys(value).length === 0) {
+      return next(requestError(400, 'Missing fields'));
+    }
+
     if (error) {
       return next(requestError(400, error.details[0].message));
     }
 
-    if (Object.keys(value).length === 0) {
-      return next(requestError(400, 'Missing fields'));
-    }
     next();
   };
 
